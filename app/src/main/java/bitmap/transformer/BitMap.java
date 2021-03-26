16866573e8453f1c75e829c8ac478061d9cbb0d5
Barrett <baxance@protonmail.com>
2021-03-25 20:16:29 -0700

    lab04 complete

--- app/src/main/java/bitmap/transformer/BitMap.java
+++ app/src/main/java/bitmap/transformer/BitMap.java
@@ -1,14 +1,20 @@
 package bitmap.transformer;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.lang.Object;
+import javax.imageio.ImageIO;
 
 public class BitMap {
     String inputFilePath;
     String outputFilePath;
     String transformName;
-
 
     public BitMap(String inputFilePath, String outputFilePath, String transformName) {
 
@@ -17,25 +23,88 @@ public class BitMap {
         this.transformName = transformName;
     }
 
-
-    public void printInfo(){
+    public String printInfo(){
+        String result = "This is your File location app/src/main/resources/img.bmp, this is your destination of new file app/src/main/img.bmp, this is that transformation you want white";
         System.out.println("This is your File location "+this.inputFilePath+", this is your destination of new file "+this.outputFilePath+", this is that transformation you want "+this.transformName);
+        return result;
     }
 
-    public void testFile(){
+    public void testFile() throws IOException {
         File selectedFile = new File(inputFilePath);
 
-        Scanner fileScanned;
+        BufferedImage fileScanned;
 
         try{
-            fileScanned= new Scanner(selectedFile);
+            fileScanned = ImageIO.read(selectedFile);
         } catch (FileNotFoundException exception) {
             exception.printStackTrace();
             System.out.println("File does not exist");
             return;
         }
 
-        System.out.println(fileScanned);
+        ImageIO.write(fileScanned, "bmp", new File(outputFilePath));
 
+        System.out.println(fileScanned);
+    }
+
+    public int paintWhite() throws IOException {
+
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(this.inputFilePath));
+        } catch (IOException e) {
+        }
+        int width = image.getWidth();
+        int height = image.getHeight();
+        Color white = new Color(255, 255, 255);
+        int rgb = white.getRGB();
+        for (int i=0;i<width;i++){
+            for (int j=0;j<height;j++){
+                image.setRGB(i, j, rgb);
+            }
+        }
+        ImageIO.write(image, "bmp", new File(this.outputFilePath));
+        return width;
+    }
+
+    public void paintBlack() throws IOException {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(this.inputFilePath));
+        } catch (IOException e) {
+        }
+        Color black = new Color(0, 0, 0);
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int rgb = black.getRGB();
+        for (int i=0;i<width;i++){
+            for (int j=0;j<height;j++){
+                image.setRGB(i, j, rgb);
+            }
+        }
+        ImageIO.write(image, "bmp", new File(this.outputFilePath));
+    }
+    public void paintBlackWhite() throws IOException {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(this.inputFilePath));
+        } catch (IOException e) {
+        }
+        Color white = new Color(255, 255, 255);
+        Color black = new Color(0, 0, 0);
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int rgb = black.getRGB();
+        int rgb2 = white.getRGB();
+        for (int i=0;i<width;i++){
+            for (int j=0;j<height;j++){
+                if (i>=width/2 && i>=height/2){
+                    image.setRGB(i, j, rgb2);
+                    continue;
+                }
+                image.setRGB(i, j, rgb);
+            }
+        }
+        ImageIO.write(image, "bmp", new File(this.outputFilePath));
     }
 }
